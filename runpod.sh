436f4dc0231e54b259030a743d2893d30340b584
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2024-08-22 13:36:34 +0200

    fix paths

--- runpod.sh
+++ runpod.sh
@@ -1,52 +1,35 @@
 # !/bin/bash
 
-# apt update
-# apt install -y screen vim git-lfs
-
-# Install stuff we need
+## Install stuff we need
 add-apt-repository -y ppa:rmescandon/yq
 apt update
 apt install -y screen vim git-lfs unzip yq
 
-cd /workspace
-
-echo ENV
+# Debug
 env
 
+## LOGIN HF
+huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
+
 ## INSTALL ai-toolkit
+cd /workspace
 git clone https://github.com/ostris/ai-toolkit.git
 cd ai-toolkit
 git submodule update --init --recursive
-# pip install torch
-# pip install -r requirements.txt
-# pip install -U accelerate transformers diffusers huggingface_hub #Optional, run it if you run into issues
 pip install accelerate transformers diffusers huggingface_hub torchvision safetensors lycoris-lora==1.8.3 flatten_json pyyaml oyaml tensorboard kornia invisible-watermark einops toml albumentations pydantic omegaconf k-diffusion open_clip_torch timm prodigyopt controlnet_aux==0.0.7 python-dotenv bitsandbytes hf_transfer lpips pytorch_fid optimum-quanto sentencepiece 
 
 ## SETUP ai-toolkit
-
-# Download images folder 
-# export IMAGE_ARCHIVE="https://drive.google.com/uc?export=download&id=1D7vCHhbSttZ-YAm1uABocsevDQT-fxd0"
 cd /workspace/ai-toolkit
-wget -O images.zip "${IMAGE_ARCHIVE}"
-mkdir -p images
-cd images
-unzip ../images.zip
 
-# Setup ostris/ai-toolkit config
-# export LORA_RANK=16
-# export LORA_ALPHA=16
-# export SAVE_STEPS=250
-# export BATCH_SIZE=1
-# export STEPS=10
-# export LEARNING_RATE=0.0001
-# export SEED=42
-# export QUANTIZE_MODEL=true
-# export TRIGGER_WORD="p3r5on"
+# Download images 
+wget -O images.zip "${IMAGE_ARCHIVE}"
+unzip ../images.zip -d images
+
+# Write ai-toolkit config with params passed from Colab notebook
 export FOLDER_PATH="/workspace/ai-toolkit/images"
 export MODEL_NAME="black-forest-labs/FLUX.1-dev"
 
-cd /workspace/ai-toolkit/config
-cp examples/train_lora_flux_24gb.yaml .
+cp examples/train_lora_flux_24gb.yaml config/
 
 declare -A yaml_params=(
   [config.process[0].network.linear]=LORA_RANK
@@ -64,39 +47,27 @@ declare -A yaml_params=(
 )
 
 for param in "${!yaml_params[@]}"; do
-  yq eval ".${param} = env(${yaml_params[$param]})" train_lora_flux_24gb.yaml > temp.yaml && mv temp.yaml train_lora_flux_24gb.yaml
+  yq eval ".${param} = env(${yaml_params[$param]})" config/train_lora_flux_24gb.yaml > config/temp.yaml && mv config/temp.yaml config/train_lora_flux_24gb.yaml
 done
 
-## SCHEDULE UPLOADS
-# cd /workspace/ai-toolkit/output/my_first_flux_lora_v1
+# upload config
+huggingface-cli upload $HF_REPO config/train_lora_flux_24gb.yaml
 
-# Upload samples/adapters every 3 mins 
-mkdir samples
-bash -c 'while true; do huggingface-cli upload $HF_REPO /workspace/ai-toolkit/output/my_first_flux_lora_v1/samples samples; sleep 180; done' &
-bash -c 'while true; do huggingface-cli upload $HF_REPO /workspace/ai-toolkit/output/my_first_flux_lora_v1 --include="*.safetensors"; sleep 180; done' &
-
-## TRAIN
-# log in to HF
-huggingface-cli login --token $HUGGINGFACE_TOKEN --add-to-git-credential
-
-# Upload ai-toolkit config
-huggingface-cli upload $HF_REPO /workspace/ai-toolkit/config/train_lora_flux_24gb.yaml
-
-# Upload logs every 3 mins 
+## SCHEDULE UPLOADS of samples/adapters every 3 mins 
+mkdir -p output/my_first_flux_lora_v1/samples
 touch ai-toolkit.log
+bash -c 'while true; do huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1/samples samples; sleep 180; done' &
+bash -c 'while true; do huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors"; sleep 180; done' &
 bash -c 'while true; do huggingface-cli upload $HF_REPO ai-toolkit.log; sleep 180; done' &
 
-# Start ai-toolkit
-cd /workspace/ai-toolkit
+## TRAIN
 python run.py config/train_lora_flux_24gb.yaml | tee ai-toolkit.log
 
-## UPLOAD RESULT
-# export HF_REPO="g-ronimo/FLUX1-dev-LoRA"
-cd /workspace/ai-toolkit/output/my_first_flux_lora_v1
-# huggingface-cli upload $HF_REPO config.yaml 
-huggingface-cli upload $HF_REPO samples samples
-huggingface-cli upload $HF_REPO . --include="*.safetensors"
+## UPLOAD RESULTS
+huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1/samples samples
+huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors"
+huggingface-cli upload $HF_REPO ai-toolkit.log
 
-# sleep infinity
-runpodctl remove pod $RUNPOD_POD_ID
+sleep infinity
+# runpodctl remove pod $RUNPOD_POD_ID
 
