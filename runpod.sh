9f1b87a256330f55ebef0abdfd7f1fa7c11f0642
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2024-08-22 16:49:26 +0200

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -57,8 +57,8 @@ huggingface-cli upload $HF_REPO config/train_lora_flux_24gb.yaml
 mkdir -p output/my_first_flux_lora_v1/samples
 touch ai-toolkit.log
 huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1/samples samples --every=3 &
-huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors" --every=3 &
-huggingface-cli upload $HF_REPO ai-toolkit.log --every=3 &
+sleep 5; huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors" --every=3 &
+sleep 5; huggingface-cli upload $HF_REPO ai-toolkit.log --every=3 &
 
 ## TRAIN
 python run.py config/train_lora_flux_24gb.yaml 2>&1 | tee ai-toolkit.log
