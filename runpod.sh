449bf5590459c765d3afa2e28fb27d264e16d7c8
geronimi73 <141400217+geronimi73@users.noreply.github.com>
2024-08-22 17:09:54 +0200

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -55,11 +55,13 @@ huggingface-cli upload $HF_REPO config/train_lora_flux_24gb.yaml
 
 ## SCHEDULE UPLOADS of samples/adapters every 3 mins 
 mkdir -p output/my_first_flux_lora_v1/samples
-touch output/my_first_flux_lora_v1/samples/test
 touch ai-toolkit.log
-huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1/samples samples --every=3 &
-sleep 5; huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors" --every=3 &
-sleep 5; huggingface-cli upload $HF_REPO ai-toolkit.log --every=3 &
+
+huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1 --include="*.safetensors" --every=3 &
+huggingface-cli upload $HF_REPO ai-toolkit.log --every=3 &
+
+# (for some reason --every does not uploady anything at all with the samples dir, no error, no idea -> bash loop)
+bash -c 'while true; do huggingface-cli upload $HF_REPO output/my_first_flux_lora_v1/samples samples; sleep 180; done' &
 
 ## TRAIN
 python run.py config/train_lora_flux_24gb.yaml 2>&1 | tee ai-toolkit.log
