b107cebea4a33d932a2dfcb84341b0a547ef8591
Isaac David <49834299+zaddy6@users.noreply.github.com>
2024-09-12 11:30:16 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Debug
+env
+
+## INSTALL ai-toolkit and other stuff
+cd /workspace
+git clone https://github.com/ostris/ai-toolkit.git
+cd ai-toolkit
+
+## SETUP ai-toolkit
+cd /workspace/ai-toolkit
+ls
